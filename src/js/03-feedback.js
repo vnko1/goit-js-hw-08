a1dfb8130e1ad9296e98b0fe10a067d039718188
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-25 14:36:36 +0200

    save

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const inputEl = form.querySelector('input');
+const textAreaEl = form.querySelector('textarea');
+
+const localStorageData = {};
+const dataStore = {};
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+document.addEventListener('DOMContentLoaded', checkingLocaleStorage);
+form.addEventListener('input', throttle(localStorageUpdate, 500));
+form.addEventListener('submit', retrievingData);
+
+function localStorageUpdate({ target }) {
+  if (target.getAttribute('type') === 'email') {
+    localStorageData.email = target.value;
+  } else {
+    localStorageData.message = target.value;
+  }
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(localStorageData));
+}
+
+function checkingLocaleStorage() {
+  const localStorageObj = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  inputEl.value = localStorageObj.email || '';
+  textAreaEl.value = localStorageObj.message || '';
+}
+
+function retrievingData(e) {
+  e.preventDefault();
+  const { email, message } = e.target.elements;
+  dataStore.email = email.value;
+  dataStore.message = message.value;
+  form.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  console.log(dataStore);
+}

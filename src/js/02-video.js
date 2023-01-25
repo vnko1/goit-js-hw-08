9c38a09ad4d6a945faea1b6461823ad264e5da99
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-25 11:38:43 +0200

    a

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}

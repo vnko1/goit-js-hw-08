193906275f3f5025cef01dd5ce259528bb6ea2cb
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-25 16:13:22 +0200

    save

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,9 +4,11 @@ const form = document.querySelector('.feedback-form');
 const inputEl = form.querySelector('input');
 const textAreaEl = form.querySelector('textarea');
 
-const localStorageData = {};
 const dataStore = {};
 const LOCALSTORAGE_KEY = 'feedback-form-state';
+const localStorageData = { email: '', message: '' };
+// console.log(localStorage);
+// console.log(localStorageData);
 
 document.addEventListener('DOMContentLoaded', checkingLocaleStorage);
 form.addEventListener('input', throttle(localStorageUpdate, 500));
@@ -15,7 +17,7 @@ form.addEventListener('submit', retrievingData);
 function localStorageUpdate({ target }) {
   if (target.getAttribute('type') === 'email') {
     localStorageData.email = target.value;
-  } else {
+  } else if (target.getAttribute('name') === 'message') {
     localStorageData.message = target.value;
   }
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(localStorageData));
@@ -23,6 +25,7 @@ function localStorageUpdate({ target }) {
 
 function checkingLocaleStorage() {
   const localStorageObj = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  console.log(localStorage);
   inputEl.value = localStorageObj.email || '';
   textAreaEl.value = localStorageObj.message || '';
 }

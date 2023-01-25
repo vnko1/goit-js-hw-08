db90652b851707c9d291efbf2b42f4a4870a363f
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-26 00:09:03 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,14 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+  player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+}
 
 function onPlay(data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
 }
